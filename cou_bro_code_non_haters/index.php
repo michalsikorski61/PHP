bcd41beb385afbf3f0ab06e6ba644a75c1ccac31
Michał Sikorski <michalsikorski61@gmail.com>
2024-09-01 10:18:22 +0200

    bro variables

--- cou_bro_code_non_haters/index.php
+++ cou_bro_code_non_haters/index.php
@@ -1,21 +1,39 @@
 <?php
-echo 'I love pizza <br>';
-echo 'It\'s really good <br>';
-// this is a comment
-/* This
-is
-a 
-multiline
-comment */
+    //variable - a reusable container that holds a data,
+    //      string, number, array, object, function, etc.
+//str
+$name = 'Michal Kowalski';
+$food = 'pizza';
+$email = 'fake123@gmail.com';
+//int
+$age = 21;
+$usr_online = 3;
+$quantity = 3;
+//float
+$gpa = 3.5;
+$price = 4.99;
+$tax_rate = 5.1;
+
+//boolen - true or false
+$employed = true;
+$online = false;
+$for_sale = true;
+
+
+
+// echo "Hello {$name}!<br>"; 
+// echo "You like {$food}!<br>";
+// echo "Your email is {$email}!<br>";
+// echo "You are {$age} years old!<br>";
+// echo "There are {$usr_online} users online!<br>";
+// echo "You would like to buy {$quantity} items!<br>";
+// echo "Your GPA is {$gpa}!<br>";
+// echo "Your pizza is \${$price}!<br>";
+// echo "The sales tax rate is {$tax_rate}%!<br>";
+// echo "Online status: {$online}<br>";
+$total = null;
+echo "You have ordered {$quantity} x {$food}!<br>";
+$total = $quantity * $price;
+echo "Your total is \${$total}!<br>";
+
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <button>Order a pizza</button>
-</body>
-</html>
