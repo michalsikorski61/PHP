db48a380f0ab525e841ed8d381e741424ab4efca
Michał Sikorski <michalsikorski61@gmail.com>
2024-09-01 04:31:50 +0200

    bro intro to php

---
+++ cou_bro_code_non_haters/index.php
@@ -0,0 +1,21 @@
+<?php
+echo 'I love pizza <br>';
+echo 'It\'s really good <br>';
+// this is a comment
+/* This
+is
+a 
+multiline
+comment */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <button>Order a pizza</button>
+</body>
+</html>
