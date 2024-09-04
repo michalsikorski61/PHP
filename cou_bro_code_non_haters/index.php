a37ce575c47827fd9e1b1e020ca9dd1c0c50deb4
Michał Sikorski <michalsikorski61@gmail.com>
2024-09-04 07:32:44 +0200

    bro basic arythmetic

--- cou_bro_code_non_haters/index.php
+++ cou_bro_code_non_haters/index.php
@@ -1,39 +1,39 @@
 <?php
-    //variable - a reusable container that holds a data,
-    //      string, number, array, object, function, etc.
-//str
-$name = 'Michal Kowalski';
-$food = 'pizza';
-$email = 'fake123@gmail.com';
-//int
-$age = 21;
-$usr_online = 3;
-$quantity = 3;
-//float
-$gpa = 3.5;
-$price = 4.99;
-$tax_rate = 5.1;
+ //Artmetic operators
+ // + - * / % **
 
-//boolen - true or false
-$employed = true;
-$online = false;
-$for_sale = true;
+ $x = 10;
+$y = 2;
+$z = null;
 
+$z = $x + $y;
+$z = $x - $y;
+$z = $x * $y;
+$z = $x / $y;
+$z = $x ** $y;
+$z = $x % $y;
+echo $z . "<br>";
+ //increment and decrement operators
+    //++ --
+echo 'increment and decrement operators' . "(counter = 0)<br>";
+$counter = 0;
+echo '$counter++' . "<br>";
+$counter++; //1
+echo $counter . "<br>";
+echo '++$counter' . "<br>";
+++$counter; //2
+echo $counter . "<br>";
+echo '$counter-=2' . "<br>";
+$counter-=2; //0
+echo $counter . "<br>";
+echo '$counter--' . "<br>";
+$counter--; //-1
+echo $counter . "<br>";
 
-
-// echo "Hello {$name}!<br>"; 
-// echo "You like {$food}!<br>";
-// echo "Your email is {$email}!<br>";
-// echo "You are {$age} years old!<br>";
-// echo "There are {$usr_online} users online!<br>";
-// echo "You would like to buy {$quantity} items!<br>";
-// echo "Your GPA is {$gpa}!<br>";
-// echo "Your pizza is \${$price}!<br>";
-// echo "The sales tax rate is {$tax_rate}%!<br>";
-// echo "Online status: {$online}<br>";
-$total = null;
-echo "You have ordered {$quantity} x {$food}!<br>";
-$total = $quantity * $price;
-echo "Your total is \${$total}!<br>";
-
-?>
+//operator precedence
+// ()
+// **
+// * / %
+// + -
+$total = 1 + 2 - 3 * 4 / 5 ** 6;
+echo $total . "<br>";
