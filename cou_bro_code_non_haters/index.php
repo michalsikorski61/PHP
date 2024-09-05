566cef9072d7d68b6f4683f126e5b6d2d38eee89
Michał Sikorski <michalsikorski61@gmail.com>
2024-09-05 15:44:52 +0200

    get and post

--- cou_bro_code_non_haters/index.php
+++ cou_bro_code_non_haters/index.php
@@ -1,39 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- <form action="<?= $_SERVER['PHP_SELF'] ?>" method="post">
+        <div>
+            <label for="login">Login</label>
+            <input type="text" name="login" id="login"></div>
+        <div>
+            <label for="password">Password</label>
+            <input type="password" name="password" id="passwrod">
+        </div>
+        <input type="submit" value="Log in">
+    </form> -->
+    <form action="<?=$_SERVER['PHP_SELF']?>" method="post">
+        <label for="quantity">quantity:</label><br/>
+        <input type="text" name="quantity">
+        <input type="submit" name="total" id="total">
+
+    </form>
+</body>
+</html>
 <?php
- //Artmetic operators
- // + - * / % **
+    $item = 'pizza';
+    $price = 5.99;
+    $quantity = $_POST['quantity'];
+    echo "You ordered {$quantity} x {$item}/s. Your total is: \$" . $quantity * $price;
 
- $x = 10;
-$y = 2;
-$z = null;
+// $_POST, $_POST - special vaiables used to collect ddata from an HTML form
+    // data is sent to the file in the action attribute of the form tag
+    // <form action="some_file.php" method="post">
 
-$z = $x + $y;
-$z = $x - $y;
-$z = $x * $y;
-$z = $x / $y;
-$z = $x ** $y;
-$z = $x % $y;
-echo $z . "<br>";
- //increment and decrement operators
-    //++ --
-echo 'increment and decrement operators' . "(counter = 0)<br>";
-$counter = 0;
-echo '$counter++' . "<br>";
-$counter++; //1
-echo $counter . "<br>";
-echo '++$counter' . "<br>";
-++$counter; //2
-echo $counter . "<br>";
-echo '$counter-=2' . "<br>";
-$counter-=2; //0
-echo $counter . "<br>";
-echo '$counter--' . "<br>";
-$counter--; //-1
-echo $counter . "<br>";
+// $_POST - data is append to the URL
+//     NOT SECURE
+//     char limit
+//     bookmark is possible w/ values
+//     GET requests can be cached
+//     better for a search pages
 
-//operator precedence
-// ()
-// **
-// * / %
-// + -
-$total = 1 + 2 - 3 * 4 / 5 ** 6;
-echo $total . "<br>";
+
+// $_POST - data is packaged inside the body of the HTTP request
+//     MORE SECURE
+//    no data limitw
+//    cannot be bookmarked
+//     GET requests cannot be cached
+//     better for sensitive data
+
