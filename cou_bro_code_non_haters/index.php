034940033778dbbc11f56be681db5806c86ec008
Michał Sikorski <michalsikorski61@gmail.com>
2024-09-21 22:12:24 +0200

    bro ifs I

--- cou_bro_code_non_haters/index.php
+++ cou_bro_code_non_haters/index.php
@@ -1,32 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <form action="<?= $_SERVER['PHP_SELF'] ?>" method="post">
-        <label for="radius">Radius:</label>
-        <input type="text" name="radius" id="radius"><br>
-        <input type="submit" value="calculate"><br>
-    </form>
-
-</body>
-</html>
 <?php
-    $radius = $_POST['radius'] ?? null;
-    $circumference = null; //ob
-    $area = null; 
-    $volume = null;
+    //if statements - if some condition is true, do something
+    // if condition is false, do something else or don't do anything at all
+$age = 18;
 
-    $circumference = 2 * pi() * $radius;
-    $circumference = round($circumference, 2);
-    $area = pi() * pow($radius, 2);
-    $area = round($area, 2);
-
-    $volume = 4/3 * pi() * pow($radius, 3);
-    $volume = round($volume, 2);
-    echo "The circumference of the circle is {$circumference} cm <br>";
-    echo "The area of the circle is {$area} cm<sup>2</sup><br>";
-    echo "The volume of the circle is {$volume} cm<sup>3</sup>";
+if($age >= 100){
+    echo "You are too old to enter this site";
+}elseif($age <= 0){
+    echo "That wasn't a valid age";
+}elseif($age >= 18){
+    echo "You may enter this site";
+}else{
+    echo "You must be 18 years or older to enter this site";
+}
