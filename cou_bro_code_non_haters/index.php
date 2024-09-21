e51c0ee735658dc2e631c7366df0d107e945c1bf
Michał Sikorski <michalsikorski61@gmail.com>
2024-09-21 22:30:30 +0200

    bro ifs II bools

--- cou_bro_code_non_haters/index.php
+++ cou_bro_code_non_haters/index.php
@@ -1,14 +1,10 @@
 <?php
     //if statements - if some condition is true, do something
     // if condition is false, do something else or don't do anything at all
-$age = 18;
+$adult = true;
 
-if($age >= 100){
-    echo "You are too old to enter this site";
-}elseif($age <= 0){
-    echo "That wasn't a valid age";
-}elseif($age >= 18){
-    echo "You may enter this site";
+if($adult){
+    echo "You may enter the website";
 }else{
-    echo "You must be 18 years or older to enter this site";
+    echo "You must be adult to enter this website";
 }
