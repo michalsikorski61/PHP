1bfd9f00a4e472326a645911c61fd0d98550fb69
Michał Sikorski <michalsikorski61@gmail.com>
2024-09-22 19:41:23 +0200

    bro switch II days of the week

--- cou_bro_code_non_haters/index.php
+++ cou_bro_code_non_haters/index.php
@@ -2,24 +2,32 @@
     //switch - replacement to using many ifelse statements
     //  more efficient, less code to write
 
-    $grade = 'A';
-
-    switch($grade){
-        case 'A':
-            echo 'You are a superstar';
-            break;
-        case 'B':
-            echo 'You did great';
-            break;
-        case 'C':
-            echo 'You did okay';
-            break;
-        case 'D':
-            echo 'You did poorly';
-            break;
-        case 'F':
-            echo 'You failed';
-            break;
-        default:
-            echo "{$grade} is not a valid grade";
-    }
+ $date = date('l'); //get the current day of the week
+ $date = 'pizza'; //for testing
+ switch(ucfirst(strtolower($date))){
+    case 'Monday':
+        echo 'I hate Mondays';
+        break;
+    case 'Tuesday':
+        echo 'It is Taco Tuesday';
+        break;
+    case 'Wednesday':
+        echo 'The work week is half over';
+        break;
+    case 'Thursday':
+        echo 'The weekend is almost here';
+        break;
+    case 'Friday':
+        echo 'The weekend is here';
+        break;
+    case 'Saturday':
+        echo 'Time to party';
+        break;
+    case 'Sunday':
+        echo 'Time to relax';
+        break;
+    default:
+        echo "{$date} is not a valid day of the week";
+        break;
+    
+ }
