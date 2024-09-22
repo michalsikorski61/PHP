58f7593398b2fee75803a0b40f9d8c2bf1875d77
Michał Sikorski <michalsikorski61@gmail.com>
2024-09-22 21:22:17 +0200

    bro for loop I basics

--- cou_bro_code_non_haters/index.php
+++ cou_bro_code_non_haters/index.php
@@ -1,33 +1,9 @@
 <?php
-    //switch - replacement to using many ifelse statements
-    //  more efficient, less code to write
-
- $date = date('l'); //get the current day of the week
- $date = 'pizza'; //for testing
- switch(ucfirst(strtolower($date))){
-    case 'Monday':
-        echo 'I hate Mondays';
-        break;
-    case 'Tuesday':
-        echo 'It is Taco Tuesday';
-        break;
-    case 'Wednesday':
-        echo 'The work week is half over';
-        break;
-    case 'Thursday':
-        echo 'The weekend is almost here';
-        break;
-    case 'Friday':
-        echo 'The weekend is here';
-        break;
-    case 'Saturday':
-        echo 'Time to party';
-        break;
-    case 'Sunday':
-        echo 'Time to relax';
-        break;
-    default:
-        echo "{$date} is not a valid day of the week";
-        break;
-    
- }
+   // for loop - repeat some code a certain number of times
+   for($i=0; $i<51; $i+=2){
+        echo "Hello World {$i} time! <br>";
+   }
+   echo "<br>for decrementing<br>";
+    for($i=11; $i>=0; $i-=2){
+          echo "Hello World {$i} time! <br>";
+    }
