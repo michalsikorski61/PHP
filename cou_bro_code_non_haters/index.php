44b7b68ee791dd2128ebe7031df2e3f148799c82
Michał Sikorski <michalsikorski61@gmail.com>
2024-09-22 22:03:23 +0200

    bro while loop I basics

--- cou_bro_code_non_haters/index.php
+++ cou_bro_code_non_haters/index.php
@@ -1,26 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>fors</title>
-</head>
-<body>
-    <form action="<?= $_SERVER['PHP_SELF'] ?>" method="post">
-        <label for="counter">enter a number to count down from:</label>
-        <input type="number" name="counter" id="counter">
-        <input type="submit" value="start">
-    </form>
-    <?php
-        if(!(isset($_POST['counter'])) || empty($_POST['counter'])){
-            echo 'please enter a number';
-            exit;
-        }
-        $counter = $_POST['counter'] ?? 0; // null coalescing operator
+<?php
+    // while loop - do some code infinitely while some 
+    // condition ramains true
 
-        for($i = $counter; $i > 0; $i--){
-            echo $i . '<br>';
-        }
-    ?>
-</body>
-</html>
+    $counter = 0;
+    while($counter < 10){
+        $counter++;
+        echo "Counter: {$counter} <br>";
+    }
