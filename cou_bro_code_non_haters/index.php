7fd29b9b9691a3ae3ec6f1ec239f310a1f7cd338
Michał Sikorski <michalsikorski61@gmail.com>
2024-09-23 22:27:00 +0200

    bro checkboxes II food array

--- cou_bro_code_non_haters/index.php
+++ cou_bro_code_non_haters/index.php
@@ -7,40 +7,24 @@
 </head>
 <body>
     <form action="<?= $_SERVER['PHP_SELF'] ?>" method="post">
-    <input type="checkbox" name="pizza" id="pizza" value="Pizza">Pizza<br/>
-    <input type="checkbox" name="hamburger" id="hamburger" value="Hamburger">Hamburger<br/>
-    <input type="checkbox" name="hotdog" id="hotdog" value="Hotdog">Hotdog<br/>
-    <input type="checkbox" name="taco" id="taco" value="Taco">Taco<br/>
+    <input type="checkbox" name="foods[]" id="pizza" value="Pizza">Pizza<br/>
+    <input type="checkbox" name="foods[]" id="hamburger" value="Hamburger">Hamburger<br/>
+    <input type="checkbox" name="foods[]" id="hotdog" value="Hotdog">Hotdog<br/>
+    <input type="checkbox" name="foods[]" id="taco" value="Taco">Taco<br/>
     <input type="submit" name="submit" value="Ślij">
 
     </form>
     <?php
         if(isset($_POST['submit'])){
-            if(isset($_POST['pizza'])){
-                echo "You like {$_POST['pizza']}<br/>";
-            }
-            if(isset($_POST['hamburger'])){
-                echo "You like {$_POST['hamburger']}<br/>";
-            }
-            if(isset($_POST['hotdog'])){
-                echo "You like {$_POST['hotdog']}<br/>";
-            }
-            if(isset($_POST['taco'])){
-                echo "You like {$_POST['taco']}<br/>";
-            }
-            
-            if(empty($_POST['pizza'])){
-                echo "You DON'T like {$_POST['pizza']}<br/>";
-            }
-            if(empty($_POST['hamburger'])){
-                echo "You DON'T like {$_POST['hamburger']}<br/>";
-            }
-            if(empty($_POST['hotdog'])){
-                echo "You DON'T like {$_POST['hotdog']}<br/>";
-            }
-            if(empty($_POST['taco'])){
-                echo "You DON'T like {$_POST['taco']}<br/>";
-            }
+           $foods = $_POST['foods'] ?? false; // foods is an array of selected checkboxes values
+           if($foods){
+                echo "You selected: <br/>";
+                foreach($foods as $food){
+                    echo $food . "<br/>";
+                }
+           }else{
+                echo "Please select at least one food";
+           }
         }
     ?>
 </body>
