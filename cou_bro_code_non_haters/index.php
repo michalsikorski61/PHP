7f18b71bb33275358d5014026f1470dfb3e675e4
Michał Sikorski <michalsikorski61@gmail.com>
2024-09-24 00:00:59 +0200

    bro fns I basics happy birthday

--- cou_bro_code_non_haters/index.php
+++ cou_bro_code_non_haters/index.php
@@ -1,31 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>checkboxes</title>
-</head>
-<body>
-    <form action="<?= $_SERVER['PHP_SELF'] ?>" method="post">
-    <input type="checkbox" name="foods[]" id="pizza" value="Pizza">Pizza<br/>
-    <input type="checkbox" name="foods[]" id="hamburger" value="Hamburger">Hamburger<br/>
-    <input type="checkbox" name="foods[]" id="hotdog" value="Hotdog">Hotdog<br/>
-    <input type="checkbox" name="foods[]" id="taco" value="Taco">Taco<br/>
-    <input type="submit" name="submit" value="Ślij">
+<?php
+    //functions - write some code onece, reuse when you need it
+    // type () after function name invoke
+    // ex. add() subtract() multiply() divide()
+function happy_birthday($first_name, $age){
+    echo "Happy Birthday dear {$first_name}! <br>";
+    echo "Happy Birthday to you! <br>";
+    echo "Happy Birthday dear {$first_name}! <br>";
+    echo "You are {$age} years old <br><br>";
 
-    </form>
-    <?php
-        if(isset($_POST['submit'])){
-           $foods = $_POST['foods'] ?? false; // foods is an array of selected checkboxes values
-           if($foods){
-                echo "You selected: <br/>";
-                foreach($foods as $food){
-                    echo $food . "<br/>";
-                }
-           }else{
-                echo "Please select at least one food";
-           }
-        }
-    ?>
-</body>
-</html>
+
+}
+
+happy_birthday("Twilight",20);
+happy_birthday("Sparkle",21);
