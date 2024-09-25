5be127020a685a16b1b9cb3b2fce96f95cfc96fc
Michał Sikorski <michalsikorski61@gmail.com>
2024-09-25 22:11:29 +0200

    Bro include

--- cou_bro_code_non_haters/index.php
+++ cou_bro_code_non_haters/index.php
@@ -3,53 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sanitize/validation</title>
+    <title>Document</title>
 </head>
 <body>
-    <form action="<?= $_SERVER['PHP_SELF'] ?>" method="post">
-    <label for="username">Username:</label>
-    <input type="text" name="username" id="username">
-    <label for="username">Age:</label>
-    <input type="text" name="age" id="age">
-    <label for="username">email:</label>
-    <input type="text" name="email" id="email">
-    <input type="submit" name="login" value="Login">
-    </form>
+<?php
+// include() - copies the content of a file (php/html/txt)
+// and includes it in your php file
+// sections of our become reusable
+// changes only need to be made in one place
+include('header.html');
+?>
+    This is the home page <br>
+    Stuff about your home page can go here
     <?php
-        if(isset($_POST['login'])){
-            if(empty($_POST['username'])){
-                echo "Username is required";
-                return;
-            }else{
-                //first validate 
-                $age = filter_input(INPUT_POST, 'age', FILTER_VALIDATE_INT);
-                $email = filter_input(INPUT_POST,'email', FILTER_VALIDATE_EMAIL);
-                echo "Var age: ";
-                var_dump($age);
-                echo "<br>";
-                echo "Var email: ";
-                var_dump($email);
-                echo "<br>";
-                if(empty($age)){
-                    echo "Valid age is required <br>";
-                    
-                }else{
-                    echo "You are {$age} years old <br>";
-                }
-                if(empty($email)){
-                    echo "Valid email is required <br>";
-                }else{
-                    echo "Your email is {$email} <br>";
-                }
-                $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_SPECIAL_CHARS);
-                $age = filter_input(INPUT_POST, 'age', FILTER_SANITIZE_NUMBER_INT);
-                $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
-                // echo "Hello, {$username}, you are {$age} years old. Your email is {$email}";
-            }
-            
-
-            
-        }
+    include('footer.html');
     ?>
 </body>
-</html>
+</html> 
---
+++ cou_bro_code_non_haters/locations.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+include('header.html');
+?>
+This is the Locations page <br>
+Stuff about your locations page can go here
+<?php
+include('footer.html');
+?>
+</body>
+</html>
