f42bbc3bb1efa085db518ec9c36f7c7d9effb781
Michał Sikorski <michalsikorski61@gmail.com>
2024-09-25 18:35:29 +0200

    bro sanitize/validate

--- cou_bro_code_non_haters/index.php
+++ cou_bro_code_non_haters/index.php
@@ -1,40 +1,55 @@
-<?php
-$username = "Michalski9";
-$username = strtolower($username);
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sanitize/validation</title>
+</head>
+<body>
+    <form action="<?= $_SERVER['PHP_SELF'] ?>" method="post">
+    <label for="username">Username:</label>
+    <input type="text" name="username" id="username">
+    <label for="username">Age:</label>
+    <input type="text" name="age" id="age">
+    <label for="username">email:</label>
+    <input type="text" name="email" id="email">
+    <input type="submit" name="login" value="Login">
+    </form>
+    <?php
+        if(isset($_POST['login'])){
+            if(empty($_POST['username'])){
+                echo "Username is required";
+                return;
+            }else{
+                //first validate 
+                $age = filter_input(INPUT_POST, 'age', FILTER_VALIDATE_INT);
+                $email = filter_input(INPUT_POST,'email', FILTER_VALIDATE_EMAIL);
+                echo "Var age: ";
+                var_dump($age);
+                echo "<br>";
+                echo "Var email: ";
+                var_dump($email);
+                echo "<br>";
+                if(empty($age)){
+                    echo "Valid age is required <br>";
+                    
+                }else{
+                    echo "You are {$age} years old <br>";
+                }
+                if(empty($email)){
+                    echo "Valid email is required <br>";
+                }else{
+                    echo "Your email is {$email} <br>";
+                }
+                $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_SPECIAL_CHARS);
+                $age = filter_input(INPUT_POST, 'age', FILTER_SANITIZE_NUMBER_INT);
+                $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+                // echo "Hello, {$username}, you are {$age} years old. Your email is {$email}";
+            }
+            
 
-echo $username . "<br>";
-
-$username = strtoupper($username);
-echo $username . "<br>";
-
-$username = str_pad($username, 20, "*");
-echo $username . "<br>";
-
-$phone_number = "343-323-231";
-$phone_number = str_replace("-", "/", $phone_number);
-echo $phone_number . "<br>";
-$username = strrev($username);
-$username = str_replace("*", "", $username);
-$username = str_repeat($username, 2);
-echo $username . "<br>";
-$username = str_shuffle($username);
-echo $username . "<br>";
-$equals = strcmp($username, "Michalski9");
-echo $equals . "<br>";
-$lenght = strlen($username);
-echo $lenght . "<br>";
-$pos = strpos($username, "9");
-echo $pos . "<br>";
-$substr = substr($username, 0, 5);
-echo $substr . "<br>";
-//substr all after 5
-$substr = substr($username, 5);
-echo $substr . "<br>";
-//insert spaces 
-$substr = substr($username, 0, 5) . " " . substr($username, 5,8) . " " . substr($username, 8);
-echo $substr . "<br>";
-$tabstr = explode(" ", $substr);
-var_dump($tabstr);
-echo "<br>";
-$str = implode("; ", $tabstr);
-echo $str . "<br>";
+            
+        }
+    ?>
+</body>
+</html>
