7bbdbd623ded0d90ab39ba9da62c78b4ebaaffa4
Michał Sikorski <michalsikorski61@gmail.com>
2024-09-26 00:30:11 +0200

    bro cookies

--- cou_bro_code_non_haters/index.php
+++ cou_bro_code_non_haters/index.php
@@ -1,22 +1,24 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
 <?php
-// include() - copies the content of a file (php/html/txt)
-// and includes it in your php file
-// sections of our become reusable
-// changes only need to be made in one place
-include('header.html');
-?>
-    This is the home page <br>
-    Stuff about your home page can go here
-    <?php
-    include('footer.html');
-    ?>
-</body>
-</html> 
+    // cookie - info about a usr stored in a usr's web browser
+    // targeted ads, browser's preferences, 
+    // and other non-sensitive data
+    setcookie('fav_food','pizza',time() + 86400,'/'); // 1 day');
+    setcookie('fav_drink','coffee',time() + (86400*3),'/');
+    setcookie('fav_dessert','ice cream',time() + (86400*7),'/');
+
+    //read 
+    foreach($_COOKIE as $key => $value){
+        echo $key . ' => ' . $value . '<br>';
+    }
+
+    if(isset($_COOKIE['fav_food'])){
+        echo "BOY SOME {$_COOKIE['fav_food']}! <br>"; 
+    }else{
+        echo "I don't know your fav food <br>";
+    }
+
+    //delete cookie
+    // setcookie('fav_drink','coffee',time() - 0,'/');
+    setcookie('fav_food','pizza',time() - 0,'/');
+    // setcookie('fav_dessert','ice cream',time() - 0,'/');
+
