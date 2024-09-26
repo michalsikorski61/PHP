06f3075de88748298caf74ea52048173966e540a
Michał Sikorski <michalsikorski61@gmail.com>
2024-09-26 03:42:51 +0200

    bro hashing

--- cou_bro_code_non_haters/renamed.php
+++ cou_bro_code_non_haters/renamed.php
@@ -1,27 +1,12 @@
 <?php
-// $_SERVER - sgb that contains headers, paths, and script locations
-// the entries in this array are created by the web server
-// shows nearly everything you need to konw about the current web page env.
-// foreach($_SERVER as $key => $value){
-//     echo "{$key} => {$value} <br>";
-// }
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SERVER SGB</title>
-</head>
-<body>
-    <form action="<?= htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="post">
-        <input type="text" name="username" id="username">
-        <input type="submit" value="Log in" name="login">
-    </form>
-    <?php
-        if($_SERVER['REQUEST_METHOD'] === 'POST'){
-            echo "Form sended thanks to post method";
-        }
-    ?>
-</body>
-</html>
+//hashing - transforming sensitive data (password)
+// into letters, numbers and/or symbols
+// via a math process. (similar to encryption)
+// hides orginal data from 3rd parties
+$password = 'pizza123';
+$hash = password_hash($password, PASSWORD_DEFAULT);
+if(password_verify($password, $hash)){
+    echo 'Password is valid';
+}else{
+    echo 'Password is invalid';
+}
