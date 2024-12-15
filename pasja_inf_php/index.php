3ccafd993d9baddcd263cf8a006821346fd515b2
Michał Sikorski <michalsikorski61@gmail.com>
2024-12-15 08:02:26 +0100

    pasja php bakery

--- pasja_inf_php/index.php
+++ pasja_inf_php/index.php
@@ -1,15 +1,16 @@
-<!DOCTYPE html>
+<div><!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Piekarnia</title>
 </head>
 <body>
-    <?php
-        $name = 'Joanna';
-        echo "Witaj $name";
-
-    ?>
+    <h1>Zamówienie online</h1>
+    <form action="final.php" method="post">
+        <div>Ile pączków (0.99 PLN/szt)<input type="text" name="paczki" id="paczki"></div>
+        <div>Ile grzebieni (1.29 PLN/szt)<input type="text" name="grzebienie" id="grzebienie"></div>
+        <input type="submit" value="Podsumuj"></div>
+    </form>
 </body>
 </html>
