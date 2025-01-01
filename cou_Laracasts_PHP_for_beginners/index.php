8ee92f7a3014456d7d9510d56d0529df2e8aa54c
Michał Sikorski <michalsikorski61@gmail.com>
2025-01-01 01:29:34 +0100

    laracasts variables

--- cou_Laracasts_PHP_for_beginners/index.php
+++ cou_Laracasts_PHP_for_beginners/index.php
@@ -2,14 +2,15 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="refresh" content="2">
+    <!-- <meta http-equiv="refresh" content="2"> -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
 
-   <h1><?php
-    echo 'Hello, dumb!';
-   ?></h1>
+    <h1><?php
+        $greeting = 'hello';
+        echo "{$greeting} "  . 'world';
+    ?></h1>
 </body>
 </html>
