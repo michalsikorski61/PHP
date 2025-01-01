0370a375abdf16bfdaee92a119e75fecc9ba535b
Michał Sikorski <michalsikorski61@gmail.com>
2025-01-01 20:56:01 +0100

    laracast conditionals and booleans and shorthand echo

--- cou_Laracasts_PHP_for_beginners/index.php
+++ cou_Laracasts_PHP_for_beginners/index.php
@@ -5,12 +5,43 @@
     <!-- <meta http-equiv="refresh" content="2"> -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        body {
+            display: grid;
+            place-items: center;
+            height: 100vh;
+            margin: 0;
+            font-family: sans-serif;
+        }
+    </style>
 </head>
 <body>
+    <form action="<?= $_SERVER['PHP_SELF'] ?>" method="get">
+        <select name="bool" id="bool">
+            <option value="true">True</option>
+            <option value="false">False</option>
+            
+        </select>
+        <button type="submit">Wyślij</button>
+    </form>
+    <?php
+        $name = "Dark Matter";
+       if(isset($_GET['bool'])){
+           $read = $_GET['bool'] === "true" ? true : false;
+       }else{
+            $read = '';
+       }
+        
+        $message = "";
 
-    <h1><?php
-        $greeting = 'hello';
-        echo "{$greeting} "  . 'world';
-    ?></h1>
+        if($read){
+            $message = "You have read $name";
+        }else{
+            $message = "You haven't read $name";
+        }
+    ?>
+    <h1><?= $message ?></h1>
+ss
+    
 </body>
 </html>
