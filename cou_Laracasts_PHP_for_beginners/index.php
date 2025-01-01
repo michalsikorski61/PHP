b964bed07c7aaf76fb2cbae18cddd6a3df3d0a29
Michał Sikorski <michalsikorski61@gmail.com>
2025-01-01 22:03:17 +0100

    laracasts arrays

--- cou_Laracasts_PHP_for_beginners/index.php
+++ cou_Laracasts_PHP_for_beginners/index.php
@@ -5,43 +5,24 @@
     <!-- <meta http-equiv="refresh" content="2"> -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <style>
-        body {
-            display: grid;
-            place-items: center;
-            height: 100vh;
-            margin: 0;
-            font-family: sans-serif;
-        }
-    </style>
+    
 </head>
 <body>
-    <form action="<?= $_SERVER['PHP_SELF'] ?>" method="get">
-        <select name="bool" id="bool">
-            <option value="true">True</option>
-            <option value="false">False</option>
-            
-        </select>
-        <button type="submit">Wyślij</button>
-    </form>
+    <h1>Recomended Books</h1>
     <?php
-        $name = "Dark Matter";
-       if(isset($_GET['bool'])){
-           $read = $_GET['bool'] === "true" ? true : false;
-       }else{
-            $read = '';
-       }
-        
-        $message = "";
+        $books = [
+            "Do Androids Dream of Electric Sheep",
+            "The Langoliers",
+            "Hail Mary"
+        ];
 
-        if($read){
-            $message = "You have read $name";
-        }else{
-            $message = "You haven't read $name";
-        }
     ?>
-    <h1><?= $message ?></h1>
-ss
+
+    <ul>
+        <?php foreach($books as $book): ?>
+            <li><?= $book ?></li>
+        <?php endforeach; ?>
+    </ul>
     
 </body>
 </html>
