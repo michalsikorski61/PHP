9967764cab6c573fcd437eb665c00977a9473ead
Michał Sikorski <michalsikorski61@gmail.com>
2025-01-06 09:29:46 +0100

    laracasts separate login from the template step 2

---
+++ cou_Laracasts_PHP_for_beginners/index.view.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <!-- <meta http-equiv="refresh" content="2"> -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    
+</head>
+<body>
+    <h1>Recomended Books</h1>
+   
+ 
+    <ul>
+        <?php foreach($filteredBooks as $book): ?>
+                <li><a href="<?= $book['purchaseUrl'] ?>">
+                    <?= "{$book['title']} ({$book['releaseYear']}) - {$book['author']}" ?>
+                </a></li>
+        <?php endforeach; ?>
+    </ul>
+    
+</body>
+</html>
