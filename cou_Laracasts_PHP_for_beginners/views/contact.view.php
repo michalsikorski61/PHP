373fe7497bed5f641acb898fc5185bb6110add6c
Michał Sikorski <michalsikorski61@gmail.com>
2025-01-11 12:53:03 +0100

    laracasts php partials step 3 extract header and dynamic headings

--- cou_Laracasts_PHP_for_beginners/views/contact.view.php
+++ cou_Laracasts_PHP_for_beginners/views/contact.view.php
@@ -2,11 +2,7 @@
   <?php require('partials/nav.php'); ?>
 
 
-  <header class="bg-white shadow">
-    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-      <h1 class="text-3xl font-bold tracking-tight text-gray-900">Contact</h1>
-    </div>
-  </header>
+  <?php require('partials/banner.php'); ?>
   <main>
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
         <p>Now yoou are on about</p>
