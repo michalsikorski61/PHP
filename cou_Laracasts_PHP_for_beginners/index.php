c3e860553c9fe8842cf93d68db6440ff2cf9a95a
Michał Sikorski <michalsikorski61@gmail.com>
2025-01-15 21:56:11 +0100

    laracasts extract php db cls step 2 - dynamic query

--- cou_Laracasts_PHP_for_beginners/index.php
+++ cou_Laracasts_PHP_for_beginners/index.php
@@ -4,12 +4,12 @@
 require ('functions.php');
 
 class Database{
-    public function query(){
+    public function query($query){
         //connect to MySQL database
-        $dsn = "mysql:host=localhost;port=3306;dbname=serwer90089_laracastcphpbeggine;user=serwer90089_laracastcphpbeggine;password=;charset=utf8mb4";
+        $dsn = "mysql:host=localhost;port=3306;dbname=serwer90089_laracastcphpbeggine;user=serwer90089_laracastcphpbeggine;password=iWIf2AGhumU-3R1?;charset=utf8mb4";
         $pdo = new PDO($dsn); //data source name (a str that dsdeclares connection to the db)
 
-        $statement = $pdo->prepare("SELECT * FROM posts");
+        $statement = $pdo->prepare($query);
 
         $statement->execute();
 
@@ -19,7 +19,7 @@ class Database{
 }
 
 $db = new Database();
-$posts = $db->query();
+$posts = $db->query("SELECT * FROM posts");
 require 'router.php';
 
 
