68ca84d39d188851343adf2181b12acf406a37eb
Michał Sikorski <michalsikorski61@gmail.com>
2025-01-15 22:19:42 +0100

    laracasts extract php db cls step 2.1 - pdo init as require db cls

--- cou_Laracasts_PHP_for_beginners/index.php
+++ cou_Laracasts_PHP_for_beginners/index.php
@@ -4,12 +4,21 @@
 require ('functions.php');
 
 class Database{
+
+    public $connection;
+    public function __construct()
+    {
+        $dsn = "mysql:host=localhost;port=3306;dbname=serwer90089_laracastcphpbeggine;user=serwer90089_laracastcphpbeggine;password=;charset=utf8mb4";
+        
+        $this->connection = new PDO($dsn); //data source name (a str that dsdeclares connection to the db)
+    }
+
+
     public function query($query){
         //connect to MySQL database
-        $dsn = "mysql:host=localhost;port=3306;dbname=serwer90089_laracastcphpbeggine;user=serwer90089_laracastcphpbeggine;password=iWIf2AGhumU-3R1?;charset=utf8mb4";
-        $pdo = new PDO($dsn); //data source name (a str that dsdeclares connection to the db)
+       
 
-        $statement = $pdo->prepare($query);
+        $statement = $this->connection->prepare($query);
 
         $statement->execute();
 
