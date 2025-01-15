61bb6549dccdf93f5e4921af81953159022842ff
Michał Sikorski <michalsikorski61@gmail.com>
2025-01-15 22:29:47 +0100

    laracasts extract php db cls step 3 - extract

---
+++ cou_Laracasts_PHP_for_beginners/Database.php
@@ -0,0 +1,24 @@
+<?php
+class Database{
+
+    public $connection;
+    public function __construct()
+    {
+        $dsn = "mysql:host=localhost;port=3306;dbname=serwer90089_laracastcphpbeggine;user=serwer90089_laracastcphpbeggine;password=;charset=utf8mb4";
+        
+        $this->connection = new PDO($dsn); //data source name (a str that dsdeclares connection to the db)
+    }
+
+
+    public function query($query){
+        //connect to MySQL database
+       
+
+        $statement = $this->connection->prepare($query);
+
+        $statement->execute();
+
+        return $statement;
+
+    }
+}
--- cou_Laracasts_PHP_for_beginners/index.php
+++ cou_Laracasts_PHP_for_beginners/index.php
@@ -2,36 +2,12 @@
 
 // Include the functions file
 require ('functions.php');
+require ('Database.php');
 
-class Database{
-
-    public $connection;
-    public function __construct()
-    {
-        $dsn = "mysql:host=localhost;port=3306;dbname=serwer90089_laracastcphpbeggine;user=serwer90089_laracastcphpbeggine;password=;charset=utf8mb4";
-        
-        $this->connection = new PDO($dsn); //data source name (a str that dsdeclares connection to the db)
-    }
-
-
-    public function query($query){
-        //connect to MySQL database
-       
-
-        $statement = $this->connection->prepare($query);
-
-        $statement->execute();
-
-        return $statement->fetchAll(PDO::FETCH_ASSOC);
-
-    }
-}
 
 $db = new Database();
-$posts = $db->query("SELECT * FROM posts");
+$post = $db->query("SELECT * FROM posts WHERE id=1")->fetch(pdo::FETCH_ASSOC);
 require 'router.php';
 
 
-foreach($posts as $post){
-    echo "<li>{$post['title']}</li>";
-}
+dd($post['title']);
