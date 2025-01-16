473be90a8ba222ad3ba5bc1f8369b43485d91600
Michał Sikorski <michalsikorski61@gmail.com>
2025-01-16 19:54:53 +0100

    laracasts env and conf flexibility step 2 - pushed upward config

--- cou_Laracasts_PHP_for_beginners/Database.php
+++ cou_Laracasts_PHP_for_beginners/Database.php
@@ -2,15 +2,12 @@
 class Database{
 
     public $connection;
-    public function __construct()
+    public function __construct($config)
     {
-        $config = [
-            'host' => 'localhost',
-            'port' => 3306,
-            'dbname' => 'serwer90089_laracastcphpbeggine',
-            'charset' => 'utf8mb4'
-        ];
-        $dsn = "mysql:host={$config['host']};port={$config['port']};dbname={$config['dbname']};user=serwer90089_laracastcphpbeggine;password=;charset={$config['charset']}";
+        
+
+        $dsn = ('mysql:'.http_build_query($config,'',';')); // example.com?foo=bar&port=3306
+        // $dsn = "mysql:host={$config['host']};port={$config['port']};dbname={$config['dbname']};user=serwer90089_laracastcphpbeggine;password=;charset={$config['charset']}";
         
         $this->connection = new PDO($dsn,'serwer90089_laracastcphpbeggine','GR@vo87PQdXgMiJ9',[
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
@@ -22,7 +19,7 @@ class Database{
         //connect to MySQL database
        
 
-        $statement = $this->connection->prepare($query);
+        $statement = $this->connection->prepare($query); 
 
         $statement->execute();
 
--- cou_Laracasts_PHP_for_beginners/index.php
+++ cou_Laracasts_PHP_for_beginners/index.php
@@ -3,11 +3,17 @@
 // Include the functions file
 require ('functions.php');
 require ('Database.php');
+$config = [
+    'host' => 'localhost',
+    'port' => 3306,
+    'dbname' => 'serwer90089_laracastcphpbeggine',
+    'charset' => 'utf8mb4'
+];
 
+$db = new Database($config);
 
-$db = new Database();
 $post = $db->query("SELECT * FROM posts WHERE id=1")->fetchALL();
 require 'router.php';
 
 
-dd($post['title']);
+dd($post);
