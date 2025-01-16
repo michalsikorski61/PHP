31edfc8cc040b52e00df21e4462f1d53ba0e0100
Michał Sikorski <michalsikorski61@gmail.com>
2025-01-16 20:01:14 +0100

    laracasts env and conf flexibility step 2.1 - dynamic usr ,pass

--- cou_Laracasts_PHP_for_beginners/Database.php
+++ cou_Laracasts_PHP_for_beginners/Database.php
@@ -2,14 +2,14 @@
 class Database{
 
     public $connection;
-    public function __construct($config)
+    public function __construct($config,$usr='serwer90089_laracastcphpbeggine',$pass='GR@vo87PQdXgMiJ9')
     {
         
 
         $dsn = ('mysql:'.http_build_query($config,'',';')); // example.com?foo=bar&port=3306
         // $dsn = "mysql:host={$config['host']};port={$config['port']};dbname={$config['dbname']};user=serwer90089_laracastcphpbeggine;password=;charset={$config['charset']}";
         
-        $this->connection = new PDO($dsn,'serwer90089_laracastcphpbeggine','GR@vo87PQdXgMiJ9',[
+        $this->connection = new PDO($dsn,$usr,$pass,[
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
         ]); //data source name (a str that dsdeclares connection to the db)
     }
