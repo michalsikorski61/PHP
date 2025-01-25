bee769ed44b21b1764bc0a664f8ffbbfe5c6052b
Michał Sikorski <michalsikorski61@gmail.com>
2025-01-25 10:43:20 +0100

    laracasts intro validation basic remember form and check input len

--- cou_Laracasts_PHP_for_beginners/controllers/note-create.php
+++ cou_Laracasts_PHP_for_beginners/controllers/note-create.php
@@ -6,10 +6,22 @@ $db = new Database($config['database']);
 $heading = "Create a new note";
 
 if($_SERVER['REQUEST_METHOD'] === 'POST'){
-    $db->query('INSERT INTO notes (id,body,user_id) VALUES(null,:body, :user_id)',[
-        'body' => $_POST['body'],
-        'user_id' => 1
-    ]);
+    $errors = [];
+    if(strlen($_POST['body']) === 0){
+        $errors['body'] = "Please enter a note";
+    }
+
+    if(strlen($_POST['body']) > 7000){
+        $errors['body'] = "Note is too long, cannot be more than 7000 characters";
+    }
+
+
+    if(empty($errors)){
+        $db->query('INSERT INTO notes (id,body,user_id) VALUES(null,:body, :user_id)',[
+            'body' => $_POST['body'],
+            'user_id' => 1
+        ]);
+    }
 }
 
 require 'views/note-create.view.php';
--- cou_Laracasts_PHP_for_beginners/controllers/notes.php
+++ cou_Laracasts_PHP_for_beginners/controllers/notes.php
@@ -1,7 +1,8 @@
 <?php
 $config = require ('config.php');
-
 $db = new Database($config['database']);
+
+
 $heading = "My Notes";
 
 $notes =$db->query('SELECT * FROM notes WHERE user_id = 1')->get();
