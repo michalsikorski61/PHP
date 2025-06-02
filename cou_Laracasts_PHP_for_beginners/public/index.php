93c61132d65ab2cc30c66144eee2e19fd12ba2e7
Michal Sikorski <michalsikorski61@gmail.com>
2025-06-02 23:05:13 +0200

    cou_lara_php Composer and free autoloading

--- cou_Laracasts_PHP_for_beginners/public/index.php
+++ cou_Laracasts_PHP_for_beginners/public/index.php
@@ -4,27 +4,27 @@ use Core\Session;
 use Core\ValidationException;
 
 session_start();
+const BASE_PATH = __DIR__ . '/../';
+require BASE_PATH . 'vendor/autoload.php';
 
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-const BASE_PATH = __DIR__ . '/../';
 // Include the functions file
 require (BASE_PATH . 'Core/functions.php');
-spl_autoload_register(function ($class) {
-    // Zamienia "Core\Response" na "Core/Response.php"
-    $class = str_replace(['\\', '/'], DIRECTORY_SEPARATOR, $class);
+// spl_autoload_register(function ($class) {
+//     // Zamienia "Core\Response" na "Core/Response.php"
+//     $class = str_replace(['\\', '/'], DIRECTORY_SEPARATOR, $class);
     
-    // Tworzymy pełną ścieżkę do pliku
-    $file = base_path("{$class}.php");
+//     // Tworzymy pełną ścieżkę do pliku
+//     $file = base_path("{$class}.php");
 
-    if (file_exists($file)) {
-        require $file;
-    } else {
-        die("Autoload error: Class {$class} not found at {$file}");
-    }
-});
-
+//     if (file_exists($file)) {
+//         require $file;
+//     } else {
+//         die("Autoload error: Class {$class} not found at {$file}");
+//     }
+// });
 require base_path('bootstrap.php');
 $router = new \Core\Router();
 
---
+++ cou_Laracasts_PHP_for_beginners/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'michalski/cou_laracasts_php_for_beginners',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'c6cf27ec3518e4e314086a22c0cc72408c3b8046',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'michalski/cou_laracasts_php_for_beginners' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'c6cf27ec3518e4e314086a22c0cc72408c3b8046',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
