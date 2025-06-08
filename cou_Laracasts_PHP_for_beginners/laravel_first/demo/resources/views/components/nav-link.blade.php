f7361a4bb073a4e58bf1a3c58de88f614c4e70f8
Michal Sikorski <michalsikorski61@gmail.com>
2025-06-08 22:05:45 +0200

    style the current active nav link homework

--- cou_Laracasts_PHP_for_beginners/laravel_first/demo/resources/views/components/nav-link.blade.php
+++ cou_Laracasts_PHP_for_beginners/laravel_first/demo/resources/views/components/nav-link.blade.php
@@ -1,8 +1,8 @@
 @props(['active' => false, 'type' => 'a'])
 @if($type === 'a')
-    @props(['type' => 'a'])
+    <a {{ $attributes}} class="{{ $active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white'}} rounded-md px-3 py-2 text-sm font-medium " 
+    aria-current="{{ $active ? 'page' : 'false' }}">{{ $slot }}</a>
 @else
-    @props(['type' => 'button'])
+    <button {{ $attributes}} class="{{ $active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white'}} rounded-md px-3 py-2 text-sm font-medium " 
+aria-current="{{ $active ? 'page' : 'false' }}">{{ $slot }}</button>
 @endif
-<{{$type}} {{ $attributes}} class="{{ $active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white'}} rounded-md px-3 py-2 text-sm font-medium " 
-aria-current="{{ $active ? 'page' : 'false' }}">{{ $slot }}</a>
