c4759d9f93f197865c5b36ac0e6b6e903d4a87d0
Michal Sikorski <michalsikorski61@gmail.com>
2025-06-11 21:10:36 +0200

    cou_laravel_laracasts model factories

--- cou_Laracasts_PHP_for_beginners/laravel_first/demo/app/Models/Job.php
+++ cou_Laracasts_PHP_for_beginners/laravel_first/demo/app/Models/Job.php
@@ -2,7 +2,14 @@
 
 namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Job extends Model{
+    use HasFactory;
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
     protected $table = 'job_listings';
 
     protected $fillable = ['title', 'salary'];
--- cou_Laracasts_PHP_for_beginners/laravel_first/demo/database/migrations/2025_06_11_183805_create_employers_table.php
+++ cou_Laracasts_PHP_for_beginners/laravel_first/demo/database/migrations/2025_06_11_183805_create_employers_table.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Models\Employer;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -12,12 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('employers', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
-            $table->text('content');
-            $table->string('author')->nullable();
-            $table->string('image')->nullable();
+            $table->string('name');
             $table->timestamps();
         });
     }
@@ -27,7 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists('employers');
     }
-
 };
