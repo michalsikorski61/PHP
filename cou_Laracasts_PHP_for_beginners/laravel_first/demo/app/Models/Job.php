cc3fa9a64144058cd1702c1a0358e979016d1c15
Michal Sikorski <michalsikorski61@gmail.com>
2025-06-11 11:08:58 +0200

    cou_laravel_laracasts introducing to migrations

--- cou_Laracasts_PHP_for_beginners/laravel_first/demo/app/Models/Job.php
+++ cou_Laracasts_PHP_for_beginners/laravel_first/demo/app/Models/Job.php
@@ -29,7 +29,7 @@ class Job{
         $job = Arr::first(static::all(), fn($job) => $job['id'] == $id);
 
         if(!$job){
-            abort(404, "Job not found");
+            abort(404);
         }
 
         return $job;
